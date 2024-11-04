7a07527bcf76c141d5e26a1373bf99bbfd92504e
Kbdlva <kabdualieva2002@gmail.com>
2024-11-04 14:37:33 +0500

    creating functions

--- tasks.py
+++ tasks.py
@@ -1,42 +1,42 @@
-num_entered = input('Enter a number: ')
+def check_number():
+    num_entered = input('Enter a number: ')
 
-try:
-    number = float(num_entered)
-    if  number >= 7:
-        print("Hello")
-except ValueError:
-    print("Invalid input.")
-
-
-
-#2
-name_input = input('Enter a name: ')
-
-try:
-    if name_input == 'John':
-        print('Hello, John')
-    else:
-        print('There is no such name')
-
-except ValueError:
-    print("Invalid input.")
+    try:
+        number = float(num_entered)
+        if  number >= 7:
+            print("Hello")
+    except ValueError:
+        print("Invalid input.")
 
 
-#3
-array = input('Enter numbers separated by spaces: ')
+def check_name():
+    name_input = input('Enter a name: ')
 
-try:
-    numbers = list(map(int, array.split()))
-    result = []
+    try:
+        if name_input == 'John':
+            print('Hello, John')
+        else:
+            print('There is no such name')
 
-    for num in numbers:
-        if num%3==0:
-            result.append(num)
+    except ValueError:
+        print("Invalid input.")
 
-    print(result)
-    
-except ValueError:
-    print("Invalid input.")
+
+def find_multiples_of_3():
+    array = input('Enter numbers separated by spaces: ')
+
+    try:
+        numbers = list(map(int, array.split()))
+        result = []
+
+        for num in numbers:
+            if num%3==0:
+                result.append(num)
+
+        print(result)
+
+    except ValueError:
+        print("Invalid input.")
 
 
 
@@ -49,3 +49,13 @@ except ValueError:
 
 # corrected 
 # [((())()(()))] or [[(())()(())]] or [(())()(())]
+
+
+
+def main():
+    check_number()
+    check_name()
+    find_multiples_of_3()
+
+if __name__ == "__main__":
+    main()
